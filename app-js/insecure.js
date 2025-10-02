79f476cac1327df01428207212b4ef93aefb695d
toshiki-hayashi-snyk <toshiki.hayashi@snyk.io>
2025-10-02 16:18:02 +0900

    Create insecure.js

---
+++ app-js/insecure.js
@@ -0,0 +1,11 @@
+// insecure.js
+const express = require('express');
+const app = express();
+
+// SQLインジェクションの例
+app.get('/user', function (req, res) {
+  db.query("SELECT * FROM users WHERE name = '" + req.query.name + "'", function (err, result) {
+    res.send(result);
+  });
+});
+app.listen(3000);
